e420f268c09543d9e9a88015ed030ca454d87673
Dominik Schlösser <dsc@dosc.net>
2021-07-11 12:13:09 +0200

    crc32 is useless for chess hashing, too many hash conflicts

--- engine/engine.h
+++ engine/engine.h
@@ -22,42 +22,6 @@ using std::wstring;
 
 #define MAX_EVAL_CACHE_ENTRIES 100000000
 
-// from: https://rosettacode.org/wiki/CRC-32#C
-uint32_t crc32(uint32_t crc, const unsigned char *buf, size_t len)
-{
-	static uint32_t table[256];
-	static int have_table = 0;
-	uint32_t rem;
-	uint8_t octet;
-	int i, j;
-	const unsigned char *p, *q;
- 
-	/* This check is not thread safe; there is no mutex. */
-	if (have_table == 0) {
-		/* Calculate CRC table. */
-		for (i = 0; i < 256; i++) {
-			rem = i;  /* remainder from polynomial division */
-			for (j = 0; j < 8; j++) {
-				if (rem & 1) {
-					rem >>= 1;
-					rem ^= 0xedb88320;
-				} else
-					rem >>= 1;
-			}
-			table[i] = rem;
-		}
-		have_table = 1;
-	}
- 
-	crc = ~crc;
-	q = buf + len;
-	for (p = buf; p < q; p++) {
-		octet = *p;  /* Cast to unsigned octet. */
-		crc = (crc >> 8) ^ table[(crc & 0xff) ^ octet];
-	}
-	return ~crc;
-}
- 
 struct EvalCacheEntry {
     int score;
     int depth;
